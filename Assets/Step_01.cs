e4dcf11a6ffc16f2832727c1b2f676b556788ddf
Jong-min Park <ss00004@naver.com>
2023-11-12 16:00:16 +0900

    first unity csharp file

---
+++ Assets/Step_01.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int myint = 10 + 10;
+        double mydouble = 10.0;
+        float myfloat = 10.0f;
+        bool mybool = true;
+        string mystring = "hello world";
+
+        Debug.Log(myint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
